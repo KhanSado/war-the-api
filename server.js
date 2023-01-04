a99c364345df2ba7b8b94dce9376e381ec3b2024
bersoncrios <bersondev@gmail.com>
2023-01-04 18:14:30 -0300

    Config and connect a mongodb

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
 
 //ROUTE FILES
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./confg/db')
 
 //Load Env vars
 dotenv.config({
     path: './confg/config.env'
 })
+
+//Connect to database
+connectDB()
+
+//ROUTE FILES
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
@@ -22,7 +29,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode ON PORT ${PORT}`)
+    console.log(`---------- Server running in ${process.env.NODE_ENV} mode ON PORT ${PORT}`.yellow.bold)
 )
+
+// Handler unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`---------- Error: ${err.message}`.red.bold);
+    server.close(() => process.exit(1))
+})
