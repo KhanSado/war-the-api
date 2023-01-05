e76fac2a42073e1520f5609b5b69b0e69790a4eb
bersoncrios <bersondev@gmail.com>
2023-01-05 12:08:13 -0300

    validations on error handler

--- middleware/error.js
+++ middleware/error.js
@@ -7,11 +7,23 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message
 
     //log for console to dev
-    console.log(err.stack.red);
+    console.log(err);
 
-    if (err.name = 'CasrError') {
+    if (err.name === 'CastError') {
         const  message = `Bootcamp not found with id of ${err.value}`
         error = new ErrorResponse(message, 404)
+    }
+
+     // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
