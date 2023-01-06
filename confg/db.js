6935deb711ee93d6b557b5c0092e24e77b710221
bersoncrios <bersonintech@gmail.com>
2023-01-06 19:08:59 -0300

    add seeder

--- confg/db.js
+++ confg/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async() => {
+    mongoose.set("strictQuery", false)
     const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`---------- MongoDB Connected: ${conn.connection.host}`.blue.bold);
 }
