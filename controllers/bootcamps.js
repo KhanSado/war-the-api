11a6bc11cd0d29af21fed18d23215fe8821f692a
bersoncrios <bersonintech@gmail.com>
2023-01-07 12:07:33 -0300

    implements pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
     const reqQuery = {...req.query}
 
-    const removeFields = ['select', 'sort']
+    const removeFields = ['select', 'sort', 'page', 'limit']
 
     removeFields.forEach(param => delete reqQuery[param])
 
@@ -35,11 +35,43 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt')
     }
 
+    //pagination
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 25
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Bootcamp.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
+    // Exacuting query
     const bootcamps = await query;
+    
+    //Pagination result
+    const pagination = {}
+
+    if (endIndex < total) {
+        pagination.next = {
+            page: page+1,
+            limit
+        }
+    }
+
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page-1,
+            limit
+        }
+    }
 
     res
         .status(200)
-        .json({success: true, count: bootcamps.length, data: bootcamps})
+        .json({
+            success: true, 
+            count: bootcamps.length, 
+            pagination,
+            data: bootcamps
+        })
     
 })
 
