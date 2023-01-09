70c2e6ac25767ae8721ec53fe87810e7ed42ffc3
bersoncrios <bersonintech@gmail.com>
2023-01-09 16:03:32 -0300

    start a logijn and add cookie parser

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async')
 const User = require('../models/User')
 
 // @desc     Register user
-// @route    GET /api/v1/auth/register
+// @route    POST /api/v1/auth/register
 // @access   Public
 exports.register = asyncHandler(async (req, res, next) => {  
     const { name, email, password, role } = req.body
@@ -15,6 +15,41 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role
     })
+
+    const token = User.getSignedJwtToken()
+
+    res
+        .status(200)
+        .json({
+            success: true,
+            token
+        })
+})
+
+// @desc     Login user
+// @route    POST /api/v1/auth/LOGIN
+// @access   Public
+exports.login = asyncHandler(async (req, res, next) => {  
+    const { email, password} = req.body
+
+    //validate email e password
+    if (!email || !password) {
+        return next(new ErrorResponse(`Please provider an email and password`, 400))
+    }
+
+    //check for user
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+        return next(new ErrorResponse(`Inválid credentials`, 401))
+    }
+
+    //check if password matches
+    const isMatch = await user.matchPassword(password)
+
+    if (!isMatch) {
+        return next(new ErrorResponse(`Inválid credentials`, 401))
+    }
 
     const token = user.getSignedJwtToken()
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
+const { Router } = require('express')
 const express = require('express')
 const { 
-    register
+    register,
+    login
 } = require('../controllers/auth')
 
 const router = express.Router()
 
 router.post('/register', register)
+router.post('/login', login)
 
 module.exports = router
