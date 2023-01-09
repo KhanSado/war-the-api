8ac84be2d84d6085eea20454fb2eb17f10e9645b
bersoncrios <bersonintech@gmail.com>
2023-01-09 16:15:03 -0300

    token in cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,14 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     })
 
-    const token = User.getSignedJwtToken()
-
-    res
-        .status(200)
-        .json({
-            success: true,
-            token
-        })
+    sendTokenResponse(user, 200, res)
 })
 
 // @desc     Login user
@@ -51,12 +44,28 @@ exports.login = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`Inválid credentials`, 401))
     }
 
-    const token = user.getSignedJwtToken()
-
-    res
-        .status(200)
-        .json({
-            success: true,
-            token
-        })
+    sendTokenResponse(user, 200, res)
 })
+
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+  
+    const options = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
+      ),
+      httpOnly: true,
+    };
+  
+    if (process.env.NODE_ENV === 'production') {
+      options.secure = true;
+    }
+  
+    res.status(statusCode).cookie('token', token, options).json({
+      success: true,
+      token,
+    });
+  };
