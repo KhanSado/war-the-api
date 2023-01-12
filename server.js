25108220efcdf7a879c71bc7babb28c36d41ef68
bersoncrios <bersonintech@gmail.com>
2023-01-12 15:15:24 -0300

    cors

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
 const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./confg/db')
 
@@ -47,6 +50,19 @@ app.use(helmet())
 
 //Prevent XSS attack
 app.use(xss())
+
+//Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 10 * 1000, //10min
+    max: 1
+})
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
+
+// Enable CORS
+app.use(cors())
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
